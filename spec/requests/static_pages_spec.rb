9250a9f163df629108ebceb02580507a13774f51
nen <your.email@example.com>
2019-11-09 18:05:32 +0900

    Add partial layouts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  include ApplicationHelper
 
   describe "GET /" do
     it "should get root" do
       get '/'
       expect(response).to have_http_status(:success)
+    end
+    it "has title 'WorkOut!!'" do
+      expect(response.body).to include full_title('')
+      expect(response.body).to_not include '| WorkOut!!'
     end
   end
 
@@ -14,12 +19,18 @@ RSpec.describe "StaticPages", type: :request do
       get '/about'
       expect(response).to have_http_status(:success)
     end
+    it "has title 'WorkOut!!について | WorkOut!!'" do
+      expect(response.body).to include full_title('WorkOut!!について')
+    end
   end
 
   describe "GET /contact" do
     it "should get contact" do
       get '/contact'
       expect(response).to have_http_status(:success)
+    end
+    it "has title 'お問い合わせ | WorkOut!!'" do
+      expect(response.body).to include full_title('お問い合わせ')
     end
   end
 
--- spec/system/site_layout_spec.rb
+++ spec/system/site_layout_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "SiteLayout", type: :system do
-  describe "layout links" do
+  xdescribe "layout links" do
     context "when not logged in" do
       before { visit root_path }
       subject { page }
@@ -11,13 +11,28 @@ RSpec.describe "SiteLayout", type: :system do
       it { is_expected.to have_link "WorkOut!!について", href: about_path }
       it { is_expected.to have_link "お問い合わせ", href: about_path }
     end
-    xcontext "when logged in" do
+    context "when logged in (admin)" do
       before { visit root_path }
       subject { page }
       it { is_expected.to have_link "WorkOut!!", href: root_path }
-      it { is_expected.to have_link "マイページ", href: user_path(user) }
+      it { is_expected.to have_link "ユーザー一覧", href: users_path }
       it { is_expected.to have_link "メニュー管理", href: menus_path }
       it { is_expected.to have_link "種目管理", href: items_path }
+      it { is_expected.to have_link "マイページ", href: user_path(user) }
+      it { is_expected.to have_link "アカウント設定", href: edit_user_path(user) }
+      it { is_expected.to have_link "ログアウト",　href: logout_path }
+      it { is_expected.to have_link "WorkOut!!について", href: about_path }
+      it { is_expected.to have_link "お問い合わせ", href: about_path }
+    end
+    context "when logged in (not admin)" do
+      before { visit root_path }
+      subject { page }
+      it { is_expected.to have_link "WorkOut!!", href: root_path }
+      it { is_expected.to_not have_link "ユーザー一覧", href: users_path }
+      it { is_expected.to have_link "メニュー管理", href: menus_path }
+      it { is_expected.to have_link "種目管理", href: items_path }
+      it { is_expected.to have_link "マイページ", href: user_path(user) }
+      it { is_expected.to have_link "アカウント設定", href: edit_user_path(user) }
       it { is_expected.to have_link "ログアウト",　href: logout_path }
       it { is_expected.to have_link "WorkOut!!について", href: about_path }
       it { is_expected.to have_link "お問い合わせ", href: about_path }
