1a0b0d044ad6f0850931d3dcb36dfaf4615985f1
nen <your.email@example.com>
2019-11-27 06:45:24 +0900

    Edit application.rb for Heroku

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module WorkoutApp
 
     #　#　以下の記述を追記する(設定必須)
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    
+    config.assets.enabled = false
   end
 end
