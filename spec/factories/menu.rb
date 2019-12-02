713e2348e4e25fe00b02e743a6425191ff9c636f
nen <your.email@example.com>
2019-12-03 06:33:27 +0900

    FInish validation test for Menu

---
+++ spec/factories/menu.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  
+  factory :munetore, class: Menu do
+    name { "胸トレ" }
+    user_id { 1 }
+  end
+end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  # メニュー作成に必要なデータを作成
+  before { create(:admin) }
+  
+  # factory_botが有効かどうかを検査
+  it "has a valid factory of menu" do
+    menu = create(:munetore)
+    expect(menu).to be_valid
+  end
+  
+  # name, user_idが無ければ無効
+  it "is invalid without name" do
+    menu = Menu.new(name: " ")
+    menu.valid?
+    expect(menu.errors[:name]).to include("を入力してください")
+  end
+  it "is invalid without user_id" do
+    menu = Menu.new(user_id: " ")
+    expect(menu).to be_invalid
+  end
+  
+  # nameが長すぎると無効
+  it "is invalid with too long name" do
+    menu = Menu.new(name: "a" * 16)
+    menu.valid?
+    expect(menu.errors[:name]).to include("は15文字以内で入力してください")
+  end
 end
