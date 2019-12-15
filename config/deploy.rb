653bbe73d015b60985c3140f9774625b4637852e
ryo <workout11app@gmail.com>
2019-12-15 19:26:01 +0900

    Edit a 'deploy.rb' file

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,19 +65,7 @@ namespace :deploy do
     end
   end
   
-  desc 'db:seed'
-  task :db_seed do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
-  
   before :starting,     :confirm
-  after  :migrate,      :db_seed
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
 end
