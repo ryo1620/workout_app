beef98e4949205aa734dd8249bcce2259130d005
ryo <workout11app@gmail.com>
2019-12-16 18:47:19 +0900

    Edit a 'contact_mailer.rb' file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -7,6 +7,6 @@ class ContactMailer < ApplicationMailer
   #
   def contact_mail(contact)
     @contact = contact  
-    mail to: ENV['MAIL'], subject: "お問い合わせ"
+    mail to: Rails.application.credentials.gmail[:user_name], subject: "お問い合わせ"
   end
 end
