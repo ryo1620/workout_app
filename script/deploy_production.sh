46546fa15158291702f4e29ed532f17212441269
ryo <workout11app@gmail.com>
2019-12-22 13:45:23 +0900

    Edit a 'deploy_prodction.sh' file in order for AWS to authorize the connection from CircleCI

--- script/deploy_production.sh
+++ script/deploy_production.sh
@@ -4,7 +4,7 @@ export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION="ap-northeast-1"
 
-MYSECURITYGROUP=”sg-02af0bc82192496d2"
+MYSECURITYGROUP="sg-02af0bc82192496d2"
 MYIP=`curl -s ifconfig.me`
 
 aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
