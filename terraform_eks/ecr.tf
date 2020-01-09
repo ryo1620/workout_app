4f48a917a74a18baa681d799263fa793b9f0cee1
ryo <workout11app@gmail.com>
2020-01-09 10:35:32 +0900

    Modify an 'awscli-kubectl' image and create a new ECR repository for it

--- terraform_eks/ecr.tf
+++ terraform_eks/ecr.tf
@@ -1,7 +1,12 @@
+# Repository
 resource "aws_ecr_repository" "workout11" {
   name = "${var.app_name}-repo"
 }
+resource "aws_ecr_repository" "awscli_kubectl" {
+  name = "awscli-kubectl"
+}
 
+# Lifecycle Policy
 resource "aws_ecr_lifecycle_policy" "workout11" {
   repository = aws_ecr_repository.workout11.name
   
@@ -10,11 +15,33 @@ resource "aws_ecr_lifecycle_policy" "workout11" {
     "rules": [
         {
             "rulePriority": 10,
-            "description": "Keep only ten untagged images, expire all others",
+            "description": "Keep only three images, expire all others",
             "selection": {
-                "tagStatus": "untagged",
+                "tagStatus": "any",
                 "countType": "imageCountMoreThan",
-                "countNumber": 10
+                "countNumber": 3
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+resource "aws_ecr_lifecycle_policy" "awscli_kubectl" {
+  repository = aws_ecr_repository.awscli_kubectl.name
+  
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 10,
+            "description": "Keep only one image, expire all others",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 1
             },
             "action": {
                 "type": "expire"
